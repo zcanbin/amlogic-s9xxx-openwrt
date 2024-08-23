0063c5e82b536c7ef0fc6ad24374249dba110d99
zcanbin <603004049@qq.com>
2024-08-23 17:29:24 +0800

    Adjust actions use repository

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -124,7 +124,7 @@ custom_packages() {
     cd packages
 
     # Download luci-app-amlogic
-    amlogic_api="https://api.github.com/repos/ophub/luci-app-amlogic/releases"
+    amlogic_api="https://api.github.com/repos/zcanbin/luci-app-amlogic/releases"
     #
     amlogic_file="luci-app-amlogic"
     amlogic_file_down="$(curl -s ${amlogic_api} | grep "browser_download_url" | grep -oE "https.*${amlogic_name}.*.ipk" | head -n 1)"
